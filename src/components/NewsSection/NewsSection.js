101bd959c31dbef3fa49440ecaea4b8f98f409aa
CurtisGrayeBabin <curtsocal@gmail.com>
2022-01-01 11:51:12 -0800

    Add Breaking News CSS updates to match Figma spec

--- src/components/NewsSection/NewsSection.js
+++ src/components/NewsSection/NewsSection.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./NewsSection.css";
 
 const NewsSection = (props) => {
-  return <section className="news">This is the news section</section>;
+  return (
+    <section className="news">
+      <h2>BREAKING NEWS!</h2>
+      <i>An 8.5 earthquake has destroyed your properties.</i>
+      <p className="result">Destroyed properties won’t earn $</p>
+    </section>
+  );
 };
 
 export default NewsSection;
